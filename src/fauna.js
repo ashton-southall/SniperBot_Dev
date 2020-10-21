20423882bbd424a649c1b80e193702c157f58a52
adsnipers <adsnipers@outlook.com>
2020-10-22 02:20:57 +1000

    notes

--- src/fauna.js
+++ src/fauna.js
@@ -3,12 +3,16 @@ var faunadb = require('faunadb'),
   
 
 var DBclient = new faunadb.Client({ secret: 'fnAD0jCjtVACCPrjyzjq5XryKsfgg638ZgrRRjPp' })
+// change secret for security reasons 
 
 
-
+// FaunaDB query
 DBclient.query(
+  // Create query
     q.Create(
+      // create new collection called 'test'
       q.Collection('test'),
+      // Insert following data in created collection
       { data: { testField: 'testValue' } }
     )
   )
