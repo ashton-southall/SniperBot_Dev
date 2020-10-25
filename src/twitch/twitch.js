f8759b66775ee9471cc5fbed1c5d16f1174503cb
Ashton Southall <38774365+Adsnipers@users.noreply.github.com>
2020-10-25 15:49:31 +1000

    return {} solution found

--- src/twitch/twitch.js
+++ src/twitch/twitch.js
@@ -1,36 +1,51 @@
 // Imports
 const tmi = require("tmi.js"); // TMI (Twitch bot library)
 const config = require('../config.json'); // Config File (Contains Secrets)
-const {Wit,log} = require('node-wit'); // WIT.AI (AI System for reading messages)
+const {
+  Wit,
+  log
+} = require('node-wit'); // WIT.AI (AI System for reading messages)
 const log4js = require('log4js'); // Log4JS (Creates logs for bot actions)
 const faunadb = require('faunadb') // FaunaDB (Data Storage)
 const q = faunadb.query;
 
-// Query DB for user info
-const fauna = new faunadb.Client({ secret: config.masterConfig.faunadb_token}); // Create FaunaDB client
-const channels = fauna.paginate(q.Match(q.Index("channels"), "true"))// Query FaunaDB database for channel list => create constant called users containing results
-channels.each(function(page) {
-  // Logs the page's contents,
-  // for example: [ Ref(Collection("test"), "1234"), ... ]
-  console.log(page);
-  const channelList = page
-});
+var channelList; // Declare channelList variable
 
-// TMI.js Options
-let options = {
-  options: {
+// Query DB for user info
+const fauna = new faunadb.Client({secret: config.masterConfig.faunadb_token}); // Create FaunaDB client
+const channels = fauna.paginate(q.Match(q.Index("channels"), "true")) // Query FaunaDB database for channel list => create constant called users containing results
+channels.each(function (page) {channelList = `${JSON.stringify(page)}`}) // Page FaunaDB results => set channelList variable to those results
+
+setTimeout(function () {
+
+  // TMI.js Options (links back to cinfig.json for most options)
+  let options = {
+    options: {
       debug: config.twitchConfig.options.debug
-  },
-  connection: {
+    },
+    connection: {
       reconnect: config.twitchConfig.options.reconnect,
       secure: config.twitchConfig.options.secure
-  },
-  identity: {
+    },
+    identity: {
       username: config.twitchConfig.connection.username,
       password: config.twitchConfig.connection.password
-  },
-  channels: page
-};
+    },
+    channels: `${channelList}` // FaunaDB query results
+  };
+
+  const TMI = new tmi.Client(options) // Create New TMI Client
+
+  // Log to confirm data loaded
+  console.log(`Admins loaded: `) // Display all admin usernames
+  console.log(`Channels loaded: ${channelList}`) // Display all channel names
+  console.log(`Blacklist loaded: `) // Display all blacklisted usernames
+
+  TMI.connect(); // Connect to twitch servers and join all channels
+
+}, 2000); // End of setTimeout function
+
+
 
 
 
@@ -52,13 +67,7 @@ log4js.configure({
 var logger = log4js.getLogger('twitch');
 logger.level = 'info';
 
-// Create Clients
-const TMI = new tmi.Client(options) // Create New TMI Client
-const AI = new Wit({accessToken: config.masterConfig.wit_token}); // Create new WIT.AI client using accessToken in config.json
-
-// Log to confirm data loaded
-console.log(`Admins loaded: `) // Display all admin usernames
-console.log(`Channels loaded: `) // Display all channel names
-console.log(`Blacklist loaded: `) // Display all blacklisted usernames
-
-TMI.connect();
+// Create AI client
+const AI = new Wit({
+  accessToken: config.masterConfig.wit_token
+}); // Create new WIT.AI client using accessToken in config.json
