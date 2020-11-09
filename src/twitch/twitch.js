63962cdc3aeb15d89723d462e96850cf579da29b
Ashton Southall <38774365+Adsnipers@users.noreply.github.com>
2020-11-09 12:45:44 +1000

    ✨ Create entry for non-existing on blacklist check

--- src/twitch/twitch.js
+++ src/twitch/twitch.js
@@ -118,9 +118,14 @@ function runMaster() {
       // Asynchronous function, check if query is finished, if not repeat check until query is finished
       async function waitForisBlacklistedResult() {
         if (typeof sender !== "undefined") {
+          console.log(`running blacklist check`)
           console.log(sender)
           if (sender[0][5] == true) {
+            console.log(`sender exists`)
             TMI.timeout(channel, tags.username, config.twitchConfig.blacklist_ban_time, config.twitchConfig.blacklist_ban_reason);
+          } else if (sender.length == 0) {
+            console.log(`Sender does not exist, creating entry`)
+            fauna.query(q.Create(q.Collection("twitch_users"), {data: {"username": tags.username,"inChannel": false,"channelName": `#${tags.username}`,"isAdmin": false,"isBlacklisted": false}}))
           }
         } else {
           setTimeout(waitForisBlacklistedResult, 250);
