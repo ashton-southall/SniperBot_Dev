c500f4cca2c2af637937d4b0eafbe31a5b830b34
Ashton Southall <38774365+Adsnipers@users.noreply.github.com>
2020-11-16 12:16:57 +1000

    💄 Code Cleanup

--- src/discord/discord.js
+++ src/discord/discord.js
@@ -4,11 +4,6 @@
 // ############################################
 // Imports
 // ##########################
-// discord.js (Discord bot library)
-// FaunaDB (Data Storage)
-// config (config.json file containing client secrets)
-// WIT.AI (AI system for interpreting messages)
-// Log4JS (Generates logs containing bot actions)
 const discordjs = require('discord.js');
 const faunadb = require('faunadb');
 const q = faunadb.query ;
@@ -18,14 +13,15 @@ const sendDM = require('./submodules/sendDM.js');
 const manualModeration = require('./submodules/manualModeration.js');
 const blacklist = require('./submodules/blacklist.js');
 const AIActions = require('./submodules/ai.js');
-const {Wit,log} = require('node-wit')
+const {Wit,log} = require('node-wit');
 
 // Create DiscordJS client
 const discord = new discordjs.Client();
 
 // Configure Dependencies
-const AI = new Wit({accessToken: SBconfig.masterConfig.witToken})
-const fauna = new faunadb.Client({secret: SBconfig.masterConfig.faunaDbToken})
+const AI = new Wit({accessToken: SBconfig.masterConfig.witToken});
+console.log(log);
+const fauna = new faunadb.Client({secret: SBconfig.masterConfig.faunaDbToken});
 
 // Set bot status on interval
 discord.once('ready', () => {setInterval(() => {discord.user.setActivity(SBconfig.discordConfig.activity, {type: SBconfig.discordConfig.activity_type})}, 10000)});
@@ -37,9 +33,9 @@ discord.login(SBconfig.discordConfig.token);
 discord.on('message', message => {
     if (message.author.bot) return
     console.log(`${message.author.id} | ${message}`);
-    if (message.channel.type == "dm") {sendDM.sendReply(discordjs, discord, message, embeds).catch(error => console.log(error));}
+    if (message.channel.type == "dm") {sendDM.sendReply(discordjs, discord, message, embeds).catch(error => console.log(error));};
     var sender
-    async function querySenderInfo() {const querySender = fauna.paginate(q.Match(q.Index("discord.users.allInfo"), message.author.id));querySender.each(function (page) {sender = page});}
+    async function querySenderInfo() {const querySender = fauna.paginate(q.Match(q.Index("discord.users.allInfo"), message.author.id));querySender.each(function (page) {sender = page});};
     querySenderInfo().catch(error => console.log(error));
     async function waitForQuery() {
         if (typeof sender !== 'undefined') {
@@ -47,10 +43,8 @@ discord.on('message', message => {
             manualModeration.purge(SBconfig, discordjs, discord, message, sender);
             manualModeration.kick(SBconfig, discordjs, discord, message, sender);
             manualModeration.ban(SBconfig, discordjs, discord, message, sender);
-            AIActions.sendMessage(AI, message, embeds.messageDeleted)
-        } else {
-            setTimeout(waitForQuery, 250);
-        }
+            AIActions.sendMessage(AI, message, embeds.messageDeleted);
+        } else {setTimeout(waitForQuery, 250);}
     }
    waitForQuery().catch(error => console.log(error));
 })
