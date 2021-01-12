73d32e6bbab83bfb6ff167669fd0984f52ab7547
adsnipers <adsnipers@outlook.com>
2021-01-12 15:06:29 +1000

    ✨ Server options - Untested

---
+++ src/discord/submodules/options.js
@@ -0,0 +1,42 @@
+async function doChannelOptions(config, discordjs, discord, message, sender, server, fauna, q) {
+    if (message.toLowerCase().startsWith(`${config.masterConfig.prefix}options`)) {
+        if (`#${tags.username}` == channel || sender[0][4] == true) {
+            var optionToChange = message.toLowerCase().split(' ')[1]
+            var optionValue = message.split(' ')[2]
+            if (typeof optionToChange !== "undefined") {
+                if (typeof optionValue !== "undefined") {
+                    if (optionToChange == 'insultthreshold') {
+                        console.log(`Notice: User updating option ${optionToChange}`);
+                        fauna.query(q.Update(q.Ref(q.Collection('discord_servers'), sender[0][0]), {data: {options: {insultThreshold: optionValue}}}, ))
+                        .catch(error => message.channel.send(`There was an error changing channel options, please double check your message, if you dont see an issue contact support at http://adfoc.us/54699276543906\r error: ${error}`).then(error => `ERROR: ${error}`))
+                        .then(message.channel.send(`Channel options changed successfully`))
+                    }
+                    else if (optionToChange == 'racismthreshold') {
+                        console.log(`Notice: User updating option ${optionToChange}`);
+                        fauna.query(q.Update(q.Ref(q.Collection('discord_servers'), sender[0][0]), {data: {options: {racismThreshold: optionValue}}}, ))
+                        .catch(error => message.channel.send(`There was an error changing channel options, please double check your message, if you dont see an issue contact support at http://adfoc.us/54699276543906\r error: ${error}`).then(error => `ERROR: ${error}`))
+                        .then(message.channel.send(`Options changed successfully`))
+                    }
+                    else if (optionToChange == 'threatthreshold') {
+                        console.log(`Notice: User updating option ${optionToChange}`);
+                        fauna.query(q.Update(q.Ref(q.Collection('discord_servers'), sender[0][0]), {data: {options: {threatThreshold: optionValue}}}, ))
+                        .catch(error => message.channel.send(`There was an error changing channel options, please double check your message, if you dont see an issue contact support at http://adfoc.us/54699276543906\r error: ${error}`).then(error => `ERROR: ${error}`))
+                        .then(message.channel.send(`Options changed successfully`))
+                    }
+                    else if (optionToChange == 'toxicitythreshold') {
+                        console.log(`Notice: User updating option ${optionToChange}`);
+                        fauna.query(q.Update(q.Ref(q.Collection('discord_servers'), sender[0][0]), {data: {options: {toxicityThreshold: optionValue}}}, ))
+                        .catch(error => message.channel.send(`There was an error changing channel options, please double check your message, if you dont see an issue contact support at http://adfoc.us/54699276543906\r error: ${error}`).then(error => `ERROR: ${error}`))
+                        .then(message.channel.send(`Options changed successfully`))
+                    }
+                }
+            } else {
+                message.channel.send(`Options for server ${server[0][0]}: insultThreshold: ${options[0][0]}, racismThreshold: ${options[0][1]}, threatThreshold: ${options[0][2]}, toxicityThreshold ${options[0][3]}`);
+            }
+        }
+    }
+}
+
+module.exports = {
+    doChannelOptions
+}
