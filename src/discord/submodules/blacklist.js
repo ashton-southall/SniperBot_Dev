1bed83aafe3a8c5b99929d82502295a14014a7bb
adsnipers <adsnipers@outlook.com>
2021-03-06 02:45:09 +1000

    🔥 Cleanup

--- src/discord/submodules/blacklist.js
+++ src/discord/submodules/blacklist.js
@@ -1,17 +1,24 @@
 async function checkBlacklist(config, discordjs, discord, message, sender) {
-        if (sender.length !== 0) {
-            if (sender[0][3] == true) {
-                message.member.kick().then(() => {
-                    console.log(`Notice: user ${message.author.id} is blacklisted, kicking from server`);
-                    message.channel.send(embeds.blacklistKick);
-                    message.author.send(embeds.blacklistDM);
-                }).catch(error => console.log(`ERROR: ${error}`))
-            }
-        } else {
-            console.log(`Notice: User ${message.author.id} is not in database, creating entry`);
-            fauna.query(q.Create(q.Collection("discord_users"), {data: {"id": message.author.id,"username": message.author.username,"isAdmin": false,"isBlacklisted": false}}))
-            .catch(error => console.log(`ERROR: ${error}`))
+    if (sender.length !== 0) {
+        if (sender[0][3] == true) {
+            message.member.kick().then(() => {
+                console.log(`Notice: user ${message.author.id} is blacklisted, kicking from server`);
+                message.channel.send(embeds.blacklistKick);
+                message.author.send(embeds.blacklistDM);
+            }).catch(error => console.log(`ERROR: ${error}`))
         }
+    } else {
+        console.log(`Notice: User ${message.author.id} is not in database, creating entry`);
+        fauna.query(q.Create(q.Collection("discord_users"), {
+                data: {
+                    "id": message.author.id,
+                    "username": message.author.username,
+                    "isAdmin": false,
+                    "isBlacklisted": false
+                }
+            }))
+            .catch(error => console.log(`ERROR: ${error}`))
+    }
 }
 
 async function manageBlacklist(config, discordjs, discord, message, sender, fauna, q) {
@@ -26,15 +33,15 @@ async function manageBlacklist(config, discordjs, discord, message, sender, faun
                 queryTarget.each(function (page) {
                     response = page
                 })
-                console.log(`Response: ${response}`)
+
                 function waitForResponse() {
                     if (typeof response !== "undefined") {
                         fauna.query(q.Update(q.Ref(q.Collection('discord_users'), response[0][0]), {
-                            data: {
-                                isBlacklisted: true
-                            }
-                        }))
-                        .then(message.channel.send(`added ${target} to the global blacklist`))
+                                data: {
+                                    isBlacklisted: true
+                                }
+                            }))
+                            .then(message.channel.send(`added ${target} to the global blacklist`))
                     } else {
                         setTimeout(waitForResponse, 250);
                     }
@@ -42,11 +49,34 @@ async function manageBlacklist(config, discordjs, discord, message, sender, faun
                 waitForResponse()
             }
         } else if (mode == 'remove') {
+            if (sender[0][2] == true) {
+                console.log(`Target: ${target}`)
+                var queryTarget = fauna.paginate(q.Match(q.Index("discord.users.allInfo"), target))
+                queryTarget.each(function (page) {
+                    response = page
+                })
 
+                function waitForResponse() {
+                    if (typeof response !== "undefined") {
+                        fauna.query(q.Update(q.Ref(q.Collection('discord_users'), response[0][0]), {
+                                data: {
+                                    isBlacklisted: false
+                                }
+                            }))
+                            .then(message.channel.send(`Removed ${target} from the global blacklist`))
+                    } else {
+                        setTimeout(waitForResponse, 250);
+                    }
+                }
+                waitForResponse()
+            }
         } else {
             // Is sender blacklisted
         }
     }
 }
 
-module.exports = {checkBlacklist, manageBlacklist}
+module.exports = {
+    checkBlacklist,
+    manageBlacklist
+}
