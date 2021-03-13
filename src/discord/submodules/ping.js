098d1828945aea981c3bfb5026bd935bdf421c8c
adsnipers <adsnipers@outlook.com>
2021-03-13 20:02:11 +1000

    🎨 enhanced ping command

--- src/discord/submodules/ping.js
+++ src/discord/submodules/ping.js
@@ -1,10 +1,9 @@
 async function get(discord, message) {
-        message.channel.send('Loading data').then(async (msg) => {
-            message.channel.send(`🏓Latency is ${msg.createdTimestamp - message.createdTimestamp}ms.`);
-            msg.delete()
-        })
-    }
+    message.channel.startTyping();
+    message.channel.send(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(discord.ws.ping)}ms`);
+    message.channel.stopTyping();
+}
 
-        module.exports = {
-            get
-        }
+module.exports = {
+    get
+}
