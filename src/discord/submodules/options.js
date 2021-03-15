045bee2f69fae6f6ffbc474dc1712dbb231803de
adsnipers <adsnipers@outlook.com>
2021-03-15 23:22:32 +1000

    ✨ Send options as embed

--- src/discord/submodules/options.js
+++ src/discord/submodules/options.js
@@ -9,40 +9,78 @@ async function doChannelOptions(config, embeds, discordjs, discord, message, sen
                     if (optionToChange.toLowerCase() == 'insultthreshold') {
                         message.channel.startTyping();
                         console.log(`Notice: User updating option ${optionToChange}`);
-                        await fauna.query(q.Update(q.Ref(q.Collection('discord_servers'), server[0][0]), {data: {options: {insultThreshold: optionValue}}}, ))
-                        .catch(error => message.channel.send(`There was an error updating channel options, please double check your message, if you dont see an issue contact support at http://adfoc.us/54699276543906\r error: ${error}`).then(error => `ERROR: ${error}`).then(console.log(`ERROR: ${error}`)))
-                        .then(message.channel.send(embeds.optionsUpdated));
+                        await fauna.query(q.Update(q.Ref(q.Collection('discord_servers'), server[0][0]), {
+                                data: {
+                                    options: {
+                                        insultThreshold: optionValue
+                                    }
+                                }
+                            }, ))
+                            .catch(error => message.channel.send(`There was an error updating channel options, please double check your message, if you dont see an issue contact support at http://adfoc.us/54699276543906\r error: ${error}`).then(error => `ERROR: ${error}`).then(console.log(`ERROR: ${error}`)))
+                            .then(message.channel.send(embeds.optionsUpdated));
                         message.channel.stopTyping();
-                    }
-                    else if (optionToChange.toLowerCase() == 'racismthreshold') {
+                    } else if (optionToChange.toLowerCase() == 'racismthreshold') {
                         message.channel.startTyping();
                         console.log(`Notice: User updating option ${optionToChange}`);
-                        await fauna.query(q.Update(q.Ref(q.Collection('discord_servers'), server[0][0]), {data: {options: {racismThreshold: optionValue}}}, ))
-                        .catch(error => message.channel.send(`There was an error updating channel options, please double check your message, if you dont see an issue contact support at http://adfoc.us/54699276543906\r error: ${error}`).then(error => `ERROR: ${error}`).then(console.log(`ERROR: ${error}`)))
-                        .then(message.channel.send(embeds.optionsUpdated));
+                        await fauna.query(q.Update(q.Ref(q.Collection('discord_servers'), server[0][0]), {
+                                data: {
+                                    options: {
+                                        racismThreshold: optionValue
+                                    }
+                                }
+                            }, ))
+                            .catch(error => message.channel.send(`There was an error updating channel options, please double check your message, if you dont see an issue contact support at http://adfoc.us/54699276543906\r error: ${error}`).then(error => `ERROR: ${error}`).then(console.log(`ERROR: ${error}`)))
+                            .then(message.channel.send(embeds.optionsUpdated));
                         message.channel.stopTyping();
-                    }
-                    else if (optionToChange.toLowerCase() == 'threatthreshold') {
+                    } else if (optionToChange.toLowerCase() == 'threatthreshold') {
                         message.channel.startTyping();
                         console.log(`Notice: User updating option ${optionToChange}`);
-                        await fauna.query(q.Update(q.Ref(q.Collection('discord_servers'), server[0][0]), {data: {options: {threatThreshold: optionValue}}}, ))
-                        .catch(error => message.channel.send(`There was an error updating channel options, please double check your message, if you dont see an issue contact support at http://adfoc.us/54699276543906\r error: ${error}`).then(error => `ERROR: ${error}`).then(console.log(`ERROR: ${error}`)))
-                        .then(message.channel.send(embeds.optionsUpdated));
+                        await fauna.query(q.Update(q.Ref(q.Collection('discord_servers'), server[0][0]), {
+                                data: {
+                                    options: {
+                                        threatThreshold: optionValue
+                                    }
+                                }
+                            }, ))
+                            .catch(error => message.channel.send(`There was an error updating channel options, please double check your message, if you dont see an issue contact support at http://adfoc.us/54699276543906\r error: ${error}`).then(error => `ERROR: ${error}`).then(console.log(`ERROR: ${error}`)))
+                            .then(message.channel.send(embeds.optionsUpdated));
                         message.channel.stopTyping();
-                    }
-                    else if (optionToChange.toLowerCase() == 'toxicitythreshold') {
+                    } else if (optionToChange.toLowerCase() == 'toxicitythreshold') {
                         message.channel.startTyping();
                         console.log(`Notice: User updating option ${optionToChange}`);
-                        await fauna.query(q.Update(q.Ref(q.Collection('discord_servers'), server[0][0]), {data: {options: {toxicityThreshold: optionValue}}}, ))
-                        .catch(error => message.channel.send(`There was an error updating channel options, please double check your message, if you dont see an issue contact support at http://adfoc.us/54699276543906\r error: ${error}`).then(error => `ERROR: ${error}`).then(console.log(`ERROR: ${error}`)))
-                        .then(message.channel.send(embeds.optionsUpdated));
+                        await fauna.query(q.Update(q.Ref(q.Collection('discord_servers'), server[0][0]), {
+                                data: {
+                                    options: {
+                                        toxicityThreshold: optionValue
+                                    }
+                                }
+                            }, ))
+                            .catch(error => message.channel.send(`There was an error updating channel options, please double check your message, if you dont see an issue contact support at http://adfoc.us/54699276543906\r error: ${error}`).then(error => `ERROR: ${error}`).then(console.log(`ERROR: ${error}`)))
+                            .then(message.channel.send(embeds.optionsUpdated));
                         message.channel.stopTyping();
                     };
                 };
             } else {
                 console.log(server[0][1]);
                 message.channel.startTyping();
-                message.channel.send(`Options for server ${server[0][1]}: insultThreshold: ${server[0][2]}, racismThreshold: ${server[0][3]}, threatThreshold: ${server[0][4]}, toxicityThreshold ${server[0][5]}`);
+                const optionsEmbed = new discordjs.MessageEmbed()
+                .setColor('#ff5757')
+                .setTitle(`Server Options`)
+                .setDescription(`Options for server: ${server[0][1]}`)
+                .addFields({
+                    name: `insultThreshold`,
+                    value: server[0][2]
+                }, {
+                    name: `racismThreshold`,
+                    value: server[0][3]
+                }, {
+                    name: `threatThreshold`,
+                    value: server[0][4]
+                }, {
+                    name: `toxicityThreshold`,
+                    value: server[0][5]
+                }).setTimestamp().setFooter('© SniperBot By Adsnipers', 'https://i.imgur.com/WFj42aM.png');
+                message.channel.send(optionsEmbed);
                 message.channel.startTyping();
             };
         };
